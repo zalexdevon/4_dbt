03e46e2bc36c44ca9ac6e96ff4515f984ffaceb2
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-07 10:28:13 +0700

    c

--- demo/read_sql.py
+++ demo/read_sql.py
@@ -1,13 +1,17 @@
 import os
 from jinja2 import Template
 from kestra import Kestra
+import sys
+
+GCP_PROJECT_ID = sys.argv[1]
+GCP_DATASET = sys.argv[2]
 
 with open("demo/demo.sql", "r") as f:
     sql_template = Template(f.read())
 
 sql = sql_template.render(
-    GCP_PROJECT_ID=os.environ.get("GCP_PROJECT_ID"),
-    GCP_DATASET=os.environ.get("GCP_DATASET"),
+    GCP_PROJECT_ID=GCP_PROJECT_ID,
+    GCP_DATASET=GCP_DATASET,
 )
 
 
