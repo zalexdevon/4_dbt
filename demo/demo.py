dadbf38ecac6dcc1a74ac8d3572bc05080dcc9db
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-07 10:02:23 +0700

    c

--- demo/demo.py
+++ demo/demo.py
@@ -8,6 +8,9 @@ csv_file = sys.argv[2]
 # Đọc file parquet
 df = pd.read_parquet(source_file)
 
+# Chỉ lấy 5 dòng đầu tiên
+df = df.head(5)
+
 # Ghi ra file CSV
 df.to_csv(csv_file, index=False)
 print(f"Shape của df: {df.shape}")
