a42b5f7c98895df38682cffec0779f86c333aca4
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-07 09:00:07 +0700

    c

--- demo/read_csv.py
+++ demo/read_csv.py
@@ -2,5 +2,5 @@ import pandas as pd
 import os
 
 # Get output của task extract
-df = outputs.extract.outputFiles["green_tripdata_2020-01.csv"]
-print(df.shape)
+# df = outputs.extract.outputFiles["green_tripdata_2020-01.csv"]
+# print(df.shape)
