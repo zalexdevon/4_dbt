5ea05b0e58da93d0a0d55d76dce5af9c6d6060bb
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-07 08:27:33 +0700

    c

--- demo/demo.py
+++ demo/demo.py
@@ -5,7 +5,8 @@ import os
 file_path = "../green_tripdata_2020-01.parquet"
 if os.path.exists(file_path):
     print("file path exists")
-
+else:
+    print("file path not exists")
 # df = pd.read_parquet(file_path)
 
 # # Ghi ra file CSV
