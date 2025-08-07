e4fc670726e8eb071d118854f243132755bcad4e
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-07 07:57:32 +0700

    c

--- demo/demo.py
+++ demo/demo.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Đọc file parquet
-df = pd.read_parquet("green_tripdata_2020-01.parquet")
+df = pd.read_parquet("../green_tripdata_2020-01.parquet")
 
 # Ghi ra file CSV
 df.to_csv("green_tripdata_2020-01.csv", index=False)
