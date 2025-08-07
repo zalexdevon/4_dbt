2c3c63803030a0152b3acf3387ee9dfa7f59ded2
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-07 08:32:35 +0700

    c

--- demo/demo.py
+++ demo/demo.py
@@ -2,14 +2,8 @@ import pandas as pd
 import os
 
 # Đọc file parquet
-print(f"Đường dẫn làm việc hiện tại: {os.getcwd()}")
-
 file_path = "green_tripdata_2020-01.parquet"
-if os.path.exists(file_path):
-    print("file path exists")
-else:
-    print("file path not exists")
-# df = pd.read_parquet(file_path)
+df = pd.read_parquet(file_path)
 
-# # Ghi ra file CSV
-# df.to_csv("green_tripdata_2020-01.csv", index=False)
+# Ghi ra file CSV
+df.to_csv("green_tripdata_2020-01.csv", index=False)
