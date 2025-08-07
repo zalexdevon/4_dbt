d5654d15725b047bcb93f268e3a650a53ce58312
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-07 09:42:07 +0700

    c

---
+++ demo/demo.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS `{{kv('GCP_PROJECT_ID')}}.{{kv('GCP_DATASET')}}.green_tripdata`
+(
+    unique_row_id BYTES,
+    filename STRING,      
+    VendorID STRING ,
+    lpep_pickup_datetime TIMESTAMP,
+    lpep_dropoff_datetime TIMESTAMP,
+    store_and_fwd_flag STRING,
+    RatecodeID STRING,
+    PULocationID STRING,
+    DOLocationID STRING,
+    passenger_count INT64,
+    trip_distance NUMERIC,
+    fare_amount NUMERIC ,
+    extra NUMERIC,
+    mta_tax NUMERIC,
+    tip_amount NUMERIC,
+    tolls_amount NUMERIC ,
+    ehail_fee NUMERIC,
+    improvement_surcharge NUMERIC,
+    total_amount NUMERIC,
+    payment_type INTEGER ,
+    trip_type STRING ,
+    congestion_surcharge NUMERIC
+)
+PARTITION BY DATE(lpep_pickup_datetime);
