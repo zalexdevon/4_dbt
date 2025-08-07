ed1d7e63a988432652e1f188989f1430f4f6537e
Minh Tam <trantam2004hocjava@gmail.com>
2025-08-07 09:02:32 +0700

    c

--- demo/read_csv.py
+++ demo/read_csv.py
@@ -4,3 +4,8 @@ import os
 # Get output của task extract
 # df = outputs.extract.outputFiles["green_tripdata_2020-01.csv"]
 # print(df.shape)
+
+if os.path.exists("green_tripdata_2020-01.parquet"):
+    print("green_tripdata_2020-01.parquet exists")
+else:
+    print("not exists")
